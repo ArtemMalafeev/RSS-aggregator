12c3dcfde3c0f6629c5573afc4fe31fb64adc676
ArtemMalafeev <ma.evgenyevich@gmail.com>
2021-10-11 18:34:46 +0300

    fix lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,58 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: 'development',
   entry: {
     main: path.resolve(__dirname, './index.js'),
   },
 
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: "[name]_bundle.js",
+    filename: '[name].bundle.js',
+  },
+
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    static: './dist',
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
   },
 
   plugins: [
-      new HtmlWebpackPlugin({
-        template: 'index.html',
-      }),
-      new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ],
 
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  }
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  },
 };
