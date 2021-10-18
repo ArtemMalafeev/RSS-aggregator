f88a1e17c38749c8c9c5ca4a4c3f74f69b72963b
ArtemMalafeev <ma.evgenyevich@gmail.com>
2021-10-18 12:13:03 +0300

    refator code

---
+++ src/view.js
@@ -0,0 +1,42 @@
+const unlockForm = (elements) => {
+  const { button, input } = elements;
+
+  button.disabled = false;
+  input.disabled = false;
+  input.focus();
+};
+
+const clearForm = ({ form }) => {
+  form.reset();
+};
+
+const blockForm = (elements) => {
+  const { button, input } = elements;
+
+  button.disabled = true;
+  input.disabled = true;
+};
+
+const renderSuccess = (i18next, elements) => {
+  const { feedback, input } = elements;
+
+  feedback.classList.replace('text-danger', 'text-success');
+  input.classList.remove('is-invalid');
+  feedback.textContent = i18next.t('successfully');
+};
+
+const renderError = (i18next, state, elements) => {
+  const { feedback, input } = elements;
+
+  feedback.classList.replace('text-success', 'text-danger');
+  input.classList.add('is-invalid');
+  feedback.textContent = i18next.t(state.addingProcess.error.key);
+};
+
+export {
+  unlockForm,
+  blockForm,
+  clearForm,
+  renderSuccess,
+  renderError,
+};
